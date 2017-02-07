0035cff504bb5c97db9f3ca4a407d341249c3fa4
ericdsmith <tech.ericsmith@gmail.com>
2017-02-06 21:06:47 -0700

    Inventory pane normalizes the price

--- client/components/ProductsPaneRow.jsx
+++ client/components/ProductsPaneRow.jsx
@@ -3,6 +3,7 @@ import {Component} from 'react'
 import material from '../assets/materialize/css/materialize.css';
 import { Link } from 'react-router';
 import index from '../assets/styles/index.css';
+import normalizePrice from '../reducers/normalizePrice';
 import classNames from 'classnames';
 
 /**
@@ -18,7 +19,7 @@ export default class ProducttdeRow extends Component {
       <tr className={classNames(index.invPane, material.collectionItem)}>
 
         <td className={material.td}><Link to={`/inventory/${this.props.row.sku}`}>{this.props.row.name}</Link></td>
-        <td> {this.props.row.price} </td>
+        <td> {normalizePrice(this.props.row.price.toString())} </td>
         <td>{this.props.row.quantity}</td>
         <td>{this.props.row.categories.sort().join(', ')}</td>
         <td>{this.props.row.sku}</td>
